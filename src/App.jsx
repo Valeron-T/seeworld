dcff34b1a386190baaea9cab2bd8348ffc045d8c
Valeron <valerontoscano@gmail.com>
2023-04-09 12:34:57 +0530

    image fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import 'animate.css';
 const App = () => (
   <div className="">
     {/* Hero section  */}
-    <div className='text-white w-full min-h-[100vh] overflow-hidden bg-cover bg-[url("./images/bayhd.jpg")] bg-left flex flex-col pb-4'>
+    <div className='text-white w-full min-h-[100vh] overflow-hidden bg-cover bg-[url("/images/bayhd.jpg")] bg-left flex flex-col pb-4'>
       <div className={`sm:px-16 px-6 flex justify-center items-center`}>
         <div className={`xl:max-w-[1280px] w-full`}>
           <Navbar />
